ecd0f40d3b8a7e1f65d6f143851b29e59517ef44
traineeEngineer <hitlersuri3@gmail.com>
2024-04-01 14:41:53 +0530

    interface in go

--- main.go
+++ main.go
@@ -2,35 +2,29 @@ package main
 
 import "fmt"
 
-type contactInfo struct {
-	email   string
-	zipCode int
+type bot interface {
+	getGreeting() string
 }
 
-type person struct {
-	firstName string
-	lastName  string
-	contactInfo
-}
+type englishBot struct{}
+type spanishBot struct{}
 
 func main() {
-	jim := person{
-		firstName: "Jim",
-		lastName:  "Party",
-		contactInfo: contactInfo{
-			email:   "jim@gmail.com",
-			zipCode: 94000,
-		},
-	}
-	jimPointer := &jim
-	jimPointer.updateName("jimmy")
-	jim.print()
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
 }
 
-func (pointerToPerson *person) updateName(newFirstName string) {
-	(*pointerToPerson).firstName = newFirstName
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
-func (p person) print() {
-	fmt.Printf("%+v", p)
+func (englishBot) getGreeting() string {
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
 }
