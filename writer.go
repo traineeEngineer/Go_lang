8bdfcd1a59e9636e9b9018679694c266f89ac988
traineeEngineer <hitlersuri3@gmail.com>
2024-04-01 15:05:04 +0530

    Create writer.go

---
+++ writer.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, resp.Body)
+
+}
